c20f610dc7c9c70511c6980395e445571bbd068c
Incogneato <wigginsmichaelc@gmail.com>
2018-04-24 15:34:52 -0400

    add resources (routes, templates, views) for todos index

--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Todo
 
 def index(request):
-    return HttpResponse('hello world')
+    todos = Todo.objects.all()[:10]
+    context = {
+        'todos': todos
+    }
+    return render(request, 'index.html', context)
