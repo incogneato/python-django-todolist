71546062eb11075d120fbc33cefa47567a35bb78
Incogneato <wigginsmichaelc@gmail.com>
2018-04-24 15:26:06 -0400

    add boolean flag for completed todo

--- todos/models.py
+++ todos/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     text  = models.TextField()
+    complete = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
